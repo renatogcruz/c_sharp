dbd09ab2ef68b66ec3e1e543729a49d4a8b34c20
renatogcruz <renatogcruz@hotmail.com>
2020-11-10 16:02:34 -0300

    c# - tipos básicos de dados

--- c_sharp_completo/2_introdução/s3_13/s3_13/Program.cs
+++ c_sharp_completo/2_introdução/s3_13/s3_13/Program.cs
@@ -7,8 +7,14 @@ namespace s3_13
         static void Main(string[] args)
         {
             sbyte x = 100;
+            byte n1 = 126; //byte não aceita números negativos
+            int n2 = 1000;
+            long n3 = 215415254L;
 
             Console.WriteLine(x);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
         }
     }
 }
