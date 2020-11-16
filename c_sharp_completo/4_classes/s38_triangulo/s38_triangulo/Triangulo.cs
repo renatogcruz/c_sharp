986f8087591819f90feecaa8be4898b9ceee9439
renatogcruz <renatogcruz@hotmail.com>
2020-11-16 10:12:40 -0300

    s.39 - criando uma classe com 3 atributos para representar melhor um triangulo

---
+++ c_sharp_completo/4_classes/s38_triangulo/s38_triangulo/Triangulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace s38_triangulo
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
