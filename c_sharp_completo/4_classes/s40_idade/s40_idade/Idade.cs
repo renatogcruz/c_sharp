118a38bd318edfc481649d55ae1abaa17b1ec2c5
renatogcruz <renatogcruz@hotmail.com>
2020-11-18 07:22:26 -0300

    s40 - primeiro exercício

---
+++ c_sharp_completo/4_classes/s40_idade/s40_idade/Idade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace s40_idade
+{
+    class Idade
+    {
+        public string Nome;
+        public int Anos;                
+    }
+}
---
+++ c_sharp_completo/4_classes/s40_idade/s40_idade/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace s40_idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Idade p1 = new Idade();
+            Idade p2 = new Idade();
+            
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.WriteLine("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p1.Anos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.WriteLine("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p2.Anos = int.Parse(Console.ReadLine());
+
+            if (p1.Anos > p2.Anos)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            }
+        }
+    }
+}
