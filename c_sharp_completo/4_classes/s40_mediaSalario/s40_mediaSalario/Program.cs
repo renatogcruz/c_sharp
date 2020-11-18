3859ccef98d17baf5238736f10a6669777bf8556
renatogcruz <renatogcruz@hotmail.com>
2020-11-18 07:41:53 -0300

    exerc média salarial

---
+++ c_sharp_completo/4_classes/s40_mediaSalario/s40_mediaSalario/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace s40_mediaSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.WriteLine("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.WriteLine("Salário: ");
+            f1.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.WriteLine("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.WriteLine("Salário: ");
+            f2.Salario = double.Parse(Console.ReadLine());
+
+            double Media = (f1.Salario + f2.Salario) / 2.0;
+            Console.WriteLine("A média salarial é de " + Media);
+           
+        }
+    }
+}
