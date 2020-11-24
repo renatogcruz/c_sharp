6e69fb25fa7c0d99f21c79b0b9440e649e19c619
renatogcruz <renatogcruz@hotmail.com>
2020-11-24 18:42:57 -0300

    c_sharp completo - Object e ToString

--- c_sharp_completo/4_classes/s40_produto/s40_produto/Produto.cs
+++ c_sharp_completo/4_classes/s40_produto/s40_produto/Produto.cs
@@ -14,5 +14,16 @@ namespace s40_produto
         {
             return Preco * Quantidade;
         }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", $ " 
+                + Preco.ToString("F2")
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2");
+        }
     }
 }
