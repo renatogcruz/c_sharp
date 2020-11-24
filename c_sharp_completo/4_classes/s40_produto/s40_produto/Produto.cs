2c7c27cb6760db6eebd2d521ecf4136579b31d0a
renatogcruz <renatogcruz@hotmail.com>
2020-11-24 17:58:05 -0300

    s40 - começando a resolver outro problema - produto

---
+++ c_sharp_completo/4_classes/s40_produto/s40_produto/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace s40_produto
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ c_sharp_completo/4_classes/s40_produto/s40_produto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace s40_produto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.WriteLine("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preco: ");
+            p.Preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+
+        }
+    }
+}
