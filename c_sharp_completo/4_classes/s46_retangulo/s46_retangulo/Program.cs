09302a24a0f7da4b563ab4e5a3e679703529f6cb
renatogcruz <renatogcruz@hotmail.com>
2020-12-04 16:10:42 -0300

    c# completo - exercícios classes

---
+++ c_sharp_completo/4_classes/s46_retangulo/s46_retangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace s46_retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            ret.Largura = double.Parse(Console.ReadLine());
+            ret.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area = " + ret.Area().ToString("F2"));
+            Console.WriteLine("Perímetro = " + ret.Perimetro().ToString("F2"));
+            Console.WriteLine("Diagonal = " + ret.Diagonal().ToString("F2"));
+        }
+    }
+}
