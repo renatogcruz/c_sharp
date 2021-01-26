3820bc1bcfa4a07bbaa94c2d0569ee091b72a220
renatogcruz <renatogcruz@hotmail.com>
2021-01-26 08:14:17 -0300

    introdução c# - módulo 1 - conhecendo a IDE VS

---
+++ introducao_ao_csharp/modulo_1/1_hello_world/PrimeiraAula/OlaMundo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Isto é um comentário
+/*
+ * está
+ * é 
+ * outra
+ * forma
+ * de
+ * comentar
+ */
+
+//Pasta lógica
+
+namespace OlaMundo
+{
+    class Program
+    {
+        // Método Main
+        //Ponto de entrada para aplicação, ou seja, é
+        //o que define o início da aplicação
+        static void Main(string[] args)
+        {
+            // Comando para imprimmir Olá, mundo
+            Console.WriteLine("Olá, Mundo");
+            Console.WriteLine("Meu nome é Renato");
+        }
+    }
+}
