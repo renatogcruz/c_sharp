b7d298bcb26bbf322962913e39ab62064bd37357
renatogcruz <renatogcruz@hotmail.com>
2021-01-30 09:29:57 -0300

    introdução ao C#

---
+++ introducao_ao_csharp/modulo_1/1_hello_world/lacos/lacos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lacos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Laços iterativos -> estruturas que repetem um bloco de código, determinado número de vezes
+
+            // Laço for
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Valor de i é " + i);
+            }
+        }
+    }
+}
