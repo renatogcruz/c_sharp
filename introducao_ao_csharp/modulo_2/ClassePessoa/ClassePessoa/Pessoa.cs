6c56e8949ebeae8c299958eaea0c3fee7d258de1
renatogcruz <renatogcruz@hotmail.com>
2021-01-31 16:03:03 -0300

    introdução c# - classes

---
+++ introducao_ao_csharp/modulo_2/ClassePessoa/ClassePessoa/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassePessoa  // Pasta virtual Classe Pessoa
+{
+    public class Pessoa // Classe carro
+    {
+        // Atributos da pessoa
+        public int Idade;
+        public string Nome;
+
+        // Comportamento da pessoa
+        public string Andar()
+        {
+            return "A pessoa esta andando";
+        }
+        public string Comer()
+        {
+            return "A pessoa esta comendo";
+        }
+    }
+}
---
+++ introducao_ao_csharp/modulo_2/ClassePessoa/ClassePessoa/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassePessoa
+{
+    class Program // Sem modificador de acesso explícito -> internal por padrão (internal class Program)
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa(); // criando pessoa
+
+            // Exibindo atributos da pessoa
+            Console.WriteLine("A pessoa tem " + pessoa.Idade + "!");
+
+            // Acessando comportamento da pessoa
+            Console.WriteLine(pessoa.Andar());
+            Console.WriteLine(pessoa.Comer());
+        }
+    }
+}
