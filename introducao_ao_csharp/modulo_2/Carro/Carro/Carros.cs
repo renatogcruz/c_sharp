30d5b692c75a4cb22b0c812250c787eb42055bb1
renatogcruz <renatogcruz@hotmail.com>
2021-02-01 09:49:18 -0300

    c# classes - criando classe carro

---
+++ introducao_ao_csharp/modulo_2/Carro/Carro/Carros.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carro         // namespace são pastas
+{
+    public class Carros // public todo mundo tem acesso
+    {
+        // Atributos do carro
+        public Cor Cor { get; set; }
+        public int Portas { get;  }
+        public string Modelo { get;  }
+        private bool ligado = false; // private -> só a classe tem acesso
+
+        public bool Ligado // Propriedade -> define uma maneira de acessar atributos
+        {
+            get // retorna valor
+            {
+                return ligado;
+            }            
+        }
+        
+        // Método construtor da classe
+        public Carros(Cor cor, int portas, string modelo)
+        {
+            Cor = cor;
+            Portas = portas;
+            Modelo = modelo;
+        }
+
+        // Comportamentos do carro
+        public string Ligar()
+        {
+            ligado = true;
+            return "O carro foi ligado.";
+        }
+        public string Desligar()
+        {
+            ligado = false;
+            return "O carro foi desligado";
+        }
+        public string Andar()
+        {
+            return "O carro está andando";
+        }
+    }
+}
---
+++ introducao_ao_csharp/modulo_2/Carro/Carro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carro
+{
+    class Program // Sem modificador de acesso explicito -> internal por padrão
+    {
+        static void Main(string[] args) // void não retorna nada
+        {
+            Carros carro = new Carros(Cor.Verde, 4, "Variante"); // criando carro
+
+            // Exibindo atributos do carro
+            Console.WriteLine("O carro é um " + carro.Modelo + " de cor " + carro.Cor + " e tem " + carro.Portas + " portas");
+                        
+            Console.WriteLine(carro.Ligar()); // ligar o carro
+            Console.WriteLine("O carro ligou? " + carro.Ligado); // verificando se o carro está realmente ligado
+            Console.WriteLine(carro.Andar()); // andar com o carro
+            Console.WriteLine(carro.Desligar());
+            Console.WriteLine("O carro ta ligado? " + carro.Ligado);
+
+            carro.Cor = Cor.Branco; // Pintando o carro de branco
+            Console.WriteLine("A nova cor do carro é " + carro.Cor);
+            
+        }
+    }
+}
