9293fb6cd4d4d0a61525a649e6afc465c732252a
renatogcruz <renatogcruz@hotmail.com>
2021-02-02 09:54:01 -0300

    c# - classe, herança e polimorfismo

---
+++ introducao_ao_csharp/modulo_2/PetShop/PetShop/Gato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop
+{
+    public class Gato : Animal
+    {
+        
+        public Gato(string nome, int idade) : base (nome, idade) // base (nome, idade) invoca
+                                                                 // o construtor da classe Animal
+        {
+           
+        }
+    }
+}
---
+++ introducao_ao_csharp/modulo_2/PetShop/PetShop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Cachorro("Rex", 5); // var -> 
+            var g = new Gato("Ana", 4);
+
+            Animal a = new Gato("Felix", 3); // Polimorfismo -> o gato também é um animal
+
+            ExibeAnimal(c); // recebe argumento do tipo cachorro
+            ExibeAnimal(g); // recebe argumento do tipo gato
+            ExibeAnimal(a); // recebe argumento do tipo animal
+            
+        }
+
+        public static void ExibeAnimal(Animal animal)
+        {
+            Console.WriteLine($"Nome: {animal.Nome}");
+            Console.WriteLine($"Nome: {animal.Idade}");
+        }
+    }
+}
