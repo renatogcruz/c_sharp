4e3aa67785a55399f25a748359f5bd2189a44080
renatogcruz <renatogcruz@hotmail.com>
2021-02-03 16:25:34 -0300

    c# - exercício proposto - interfaces

---
+++ introducao_ao_csharp/modulo_2/Interfaces/Interfaces/Multifuncional.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Multifuncional : ICopiar, IImpressora, IEscaneadora
+    {
+        public string Copiar(string texto)
+        {
+            return $"Copiando texto: {texto}";
+        }
+
+        public string Escanear(string texto)
+        {
+            return $"Escaneando texto: {texto}";
+        }
+
+        public string Imprimir(string texto)
+        {
+            return $"Imprimindo texto: {texto}";
+        }
+    }
+}
--- introducao_ao_csharp/modulo_2/Interfaces/Interfaces/Program.cs
+++ introducao_ao_csharp/modulo_2/Interfaces/Interfaces/Program.cs
@@ -13,16 +13,24 @@ namespace Interfaces
             var i = new ImpressoraComum();
             var i2 = new ImpressoraCopiadora();
             var c = new ImpressoraCopiadora();
+           
 
             Console.WriteLine(i.Imprimir("Estou estudando c#"));
             Console.WriteLine(i2.Copiar("Estou estudando C# e gostando"));
 
             CopiarDocumento(new Xerox(), "Hello, world");
+
+            EscanearDocumento(new Multifuncional(), "Escanenado");
         }
 
         public static void CopiarDocumento(ICopiar c, string texto)
         {
             Console.WriteLine("Estou copiando o texto: " + c.Copiar(texto));
+        }
+
+        public static void EscanearDocumento(IEscaneadora i, string texto)
+        {
+            Console.WriteLine("Estou copiando o texto: " + i.Escanear(texto));
         }
     }
 }
