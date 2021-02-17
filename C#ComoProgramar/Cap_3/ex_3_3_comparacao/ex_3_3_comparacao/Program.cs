0c51a1ff7705025abcd67c22c8ef24f7eb7abf47
renatogcruz <renatogcruz@hotmail.com>
2021-02-17 14:22:44 -0300

    c# como programar - programa simples 3

---
+++ C#ComoProgramar/Cap_3/ex_3_3_comparacao/ex_3_3_comparacao/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Fig. 3.19: Comparison.cs
+// Usando instruções if, operadores relacionais e operadores de igualdade
+
+namespace ex_3_3_comparacao
+{
+    class Comparison
+    {
+        static void Main(string[] args)
+        {
+            int number1, // primeiro número a comparar
+                number2; // segundo número a comparar
+
+            // lê o primeiro número do usuário
+            Console.WriteLine("Please enter first integer: ");
+            number1 = Int32.Parse(Console.ReadLine());
+
+            // lê o segundo número do usuário
+            Console.WriteLine("Please enter first integer: ");
+            number2 = Int32.Parse(Console.ReadLine());
+
+            if ( number1 == number2 )
+                Console.WriteLine(number1 + " == " + number2);
+
+            if ( number1 != number2 )
+                Console.WriteLine(number1 + " != " + number2);
+
+            if ( number1 < number2 )
+                Console.WriteLine(number1 + " < " + number2);
+
+            if ( number1 > number2 )
+                Console.WriteLine(number1 + " > " + number2);
+
+            if ( number1 <= number2 )
+                Console.WriteLine(number1 + " <= " + number2);
+
+            if ( number1 >= number2 )
+                Console.WriteLine(number1 + " >= " + number2);
+                        
+        } // fim do método Main
+    } // fim da classe Comparison
+}
