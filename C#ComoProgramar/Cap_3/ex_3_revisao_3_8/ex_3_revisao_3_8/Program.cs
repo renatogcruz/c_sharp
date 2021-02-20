d6596ced513764d7f38e4f8179c82f3af7d23564
renatogcruz <renatogcruz@hotmail.com>
2021-02-20 20:16:54 -0300

    c# como programar - exercicios revisão

---
+++ C#ComoProgramar/Cap_3/ex_3_revisao_3_8/ex_3_revisao_3_8/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_3_revisao_3_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, // primeiro número 
+                number2; // segundo número
+
+            // lê o primeiro número do usuário
+            Console.Write( "Entre com o primero número: " );
+            number1 = Int32.Parse(Console.ReadLine());
+
+            // lê o segundo número do usuário
+            Console.Write("Entre com o segundo número: ");
+            number2 = Int32.Parse(Console.ReadLine());
+
+            // soma e mostra os dois números
+            Console.WriteLine("A soma de " + number1 + 
+                " mais " + 
+                number2 + 
+                " é " + 
+                (number1 + number2) + 
+                "!");
+
+            // mostra o produto dos dois números
+            Console.WriteLine("O produto de " + number1 +
+               " x " +
+               number2 +
+               " é " +
+               (number1 * number2) +
+               "!");
+
+            // mostra a diferença dos dois números
+            Console.WriteLine("A diferença de " + number1 +
+               " menos " +
+               number2 +
+               " é " +
+               (number1 - number2) +
+               "!");
+
+            // mostra o quociente dos dois números
+            Console.WriteLine("O quociente entre " + number1 +
+               " e " +
+               number2 +
+               " é " +
+               (number1 % number2) +
+               "!");
+        }   // fim do método Main
+    }   // fim da classe Programa   
+}
---
+++ C#ComoProgramar/Cap_3/ex_3_revisao_3_9/ex_3_revisao_3_9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_3_revisao_3_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio,         // número do raio do círculo
+                   dim,          // número do diâmetro do círculo
+                   circ,         // número da circunferência do círculo
+                   area,         // número da área do círculo
+                   pi = 3.14;    // valor de Pi                
+
+            // lê o raio do usuário
+            Console.Write("Entre com o número do raio da circunfêrencia: ");
+            raio = double.Parse(Console.ReadLine());
+            
+            // diâmetro do círculo
+            dim = 2 * raio;
+            Console.WriteLine( "O diâmetro do círculo é " + dim + " !" );
+
+            // circunferência do círculo
+            circ = 2 * raio * pi;
+            Console.WriteLine( "A circunferência do círculo é " + circ + " !" );
+
+            // mostra a diferença dos dois números
+            area = pi * (Math.Pow(raio, 2));
+            Console.WriteLine( "A área do círculo é " + dim + " !" );                       
+                        
+        }  // fim do método Main
+    }   // fim da classe Programa
+}
