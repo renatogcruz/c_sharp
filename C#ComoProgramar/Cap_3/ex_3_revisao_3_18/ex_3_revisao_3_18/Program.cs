6db8df66d15b5774ec344ad518096b8b35ac27b2
renatogcruz <renatogcruz@hotmail.com>
2021-02-21 11:40:01 -0300

    c# - concatenar strings

---
+++ C#ComoProgramar/Cap_3/ex_3_revisao_3_18/ex_3_revisao_3_18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_3_revisao_3_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prenome,
+                sobrenome;
+
+            // lê o prenome
+            Console.Write( "Entre com seu prenome: " );
+            prenome = Console.ReadLine();
+
+            // lê o sobrenome
+            Console.Write("Entre com seu sobrenome: ");
+            sobrenome = Console.ReadLine();
+
+            // concatene prenome + nome e imprima resultado
+            Console.WriteLine( prenome + " " + sobrenome );
+            
+        }  // fim do método Main
+    }   // fim da classe Programa
+}
