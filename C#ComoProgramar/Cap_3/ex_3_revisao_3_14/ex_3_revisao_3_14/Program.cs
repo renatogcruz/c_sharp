db05d85f0b89bfbff6f13ffd0929728a6fe8c4dc
renatogcruz <renatogcruz@hotmail.com>
2021-02-21 11:08:04 -0300

    c# - revisão cap 3

---
+++ C#ComoProgramar/Cap_3/ex_3_revisao_3_14/ex_3_revisao_3_14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_3_revisao_3_14
+{
+    class Multiplo
+    {
+        static void Main(string[] args)
+        {
+            int number1,
+                number2;
+                
+
+            // lê o primeiro número
+            Console.Write( "Entre com o primeiro número: " );
+            number1 = Int32.Parse( Console.ReadLine() );
+
+            // lê o segundo número
+            Console.Write("Entre com o segundo número: ");
+            number2 = Int32.Parse(Console.ReadLine());
+
+            // verifique se o primeiro é multiplo o segundo
+            if ( number1 % number2 == 0)
+            {
+                Console.WriteLine( "O número {0} é multiplo do número {1}",  number1, number2 );
+            }
+            else
+            {
+                Console.WriteLine( "O número {0} não é multiplo do número {1}", number1, number2 );
+            }
+
+        }   // fim do método Main
+    }   // fim da classe Multiplo
+}
