5130a537bdff171b97ad90d80652c333bf9877b7
renatogcruz <renatogcruz@hotmail.com>
2021-02-22 11:21:03 -0300

    c# - média sala de aula

---
+++ C#ComoProgramar/Cap_4/ex_4_6_repeticao_controlada/ex_4_6_repeticao_controlada/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_4_6_repeticao_controlada
+{
+    // fig. 4.7: Average1.cs
+    // Média da turma com repetiçao controlada por contador
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total,            //
+                gradeCounter,     //
+                gradeValue,       //
+                average;          //
+
+            // fase de inicialização
+            total = 0;
+            gradeCounter = 1;
+
+            // processamento
+            while ( gradeCounter <= 10)
+            {
+                // solicita a entrada e lê a nota do usuário
+                Console.WriteLine( "Enter integer grade: " );
+
+                // lê a entrada e converte para inteiro
+                gradeValue = Int32.Parse(Console.ReadLine());
+
+                // soma gradeValue ao total
+                total = total + gradeValue;
+
+                // soma 1 a gradeCounter
+                gradeCounter = gradeCounter + 1;
+            }
+
+            // fase de conclusão
+            average = total / 10;
+
+            // exibe a média das notas do exame
+            Console.WriteLine( "\nClass average is {0}", average );
+
+        }  // fim de Main
+    }   // fim da classe Average1
+}
---
+++ C#ComoProgramar/Cap_4/nota_cap_4_1/nota_cap_4_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nota_cap_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int notaAluno;
+
+            notaAluno = 40;
+
+            // estrutura de seleção if
+            if ( notaAluno >= 60)            
+                Console.WriteLine("Aprovado.");
+            
+            // estrutura de seleção if/else
+            if (notaAluno >= 60)            
+                Console.WriteLine("Aprovado.");            
+            else           
+                Console.WriteLine("Reprovado");
+            
+
+            // if/else com expressão condicional (?)
+            Console.WriteLine(  notaAluno >= 60 ? "Aprovado.": "Reprovado." );
+
+            // multiplos elses
+            if (notaAluno >= 90)            
+                Console.WriteLine( "A" );
+                      
+            //
+            if ( notaAluno >= 60 )
+                Console.WriteLine( "Passou" );
+            else
+            {
+                Console.WriteLine( "Reprovou" );
+                Console.WriteLine( "Você terá de fazer esta matéria novamente." );
+            }
+
+            // 
+            if ( notaAluno >= 90 )
+                Console.WriteLine( "A" );
+            else if ( notaAluno >= 80)
+                Console.WriteLine( "B" );
+            else if ( notaAluno >= 70 )
+                Console.WriteLine( "C" );
+            else if ( notaAluno >= 60 )
+                Console.WriteLine( "D" );
+            else
+                Console.WriteLine( "E" );
+
+
+        }   //
+    }   //
+}
