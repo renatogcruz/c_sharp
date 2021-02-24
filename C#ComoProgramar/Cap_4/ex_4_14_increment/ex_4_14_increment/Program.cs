387d30461fb854ba3a48ae622f71da50e6ed8b5b
renatogcruz <renatogcruz@hotmail.com>
2021-02-24 10:10:48 -0300

    c# - Increment.cs program

---
+++ C#ComoProgramar/Cap_4/ex_4_14_increment/ex_4_14_increment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_4_14_increment
+{
+    // Fg. 4.14: Increment.cs
+    // Pré-incremento e pós-incremento
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+
+            c = 5;
+            Console.WriteLine( c );      // imprime 5
+            Console.WriteLine( c++ );    // imprime 5 e depois pós-incrementa
+            Console.WriteLine( c );      // imprime 6
+
+            Console.WriteLine();         // pula uma linha
+
+            c = 5;
+            Console.WriteLine( c );      // imprime 5
+            Console.WriteLine( ++c );    // pré-incremneta e depois imprime 6
+            Console.WriteLine( c );      // imprime 6
+
+        } // fim do método Main
+    } // fim da classe Increment
+}
