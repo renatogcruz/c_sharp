143f5bd1e3cb67e8ebd695ad1d05d9da407320c4
renatogcruz <renatogcruz@hotmail.com>
2021-02-26 21:10:59 -0300

    c# programa incrementar UM

---
+++ C#ComoProgramar/Cap_4/revisao_4/ex_4_somando/ex_4_somando/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_4_somando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4.3 - somando 1 à variável inteira x 
+            int x;
+
+            x = 5;
+                        
+            Console.WriteLine(x = x + 1);    // imprime a soma         
+            Console.WriteLine(x += 1);       // imprime a soma     
+            Console.WriteLine(x++);          // imprime e depois incrementa (pós-incrementa)
+            Console.WriteLine(++x);          // incrementa e depois imprime (pré-incrementa)
+                       
+        }
+    }
+}
