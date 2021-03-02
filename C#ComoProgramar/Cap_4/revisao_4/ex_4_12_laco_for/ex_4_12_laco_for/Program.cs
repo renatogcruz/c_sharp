9f0bacb74a8396f68c7be9117cf292649460eb79
renatogcruz <renatogcruz@hotmail.com>
2021-03-02 09:01:03 -0300

    c# estruturas de repetição

---
+++ C#ComoProgramar/Cap_4/revisao_4/ex_4_12_laco_for/ex_4_12_laco_for/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_4_12_laco_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            N = 1;
+
+            while (N <= 5)
+            {
+                Console.WriteLine("N = " + N );                
+                N += 1;
+            }
+
+            // 4.13 x = 9 e y = 11
+            int x,
+                y;
+
+            x = 9;
+            y = 11;
+
+            if (x < 10)
+                if (y > 10)
+                    Console.WriteLine( "********" );
+                else
+                    Console.WriteLine( "#######" );
+                    Console.WriteLine( "$$$$$$$" );
+
+
+            if (x < 10)
+            {
+                if (y > 10)
+                    Console.WriteLine( "********" );
+            }
+            else
+            {
+                Console.WriteLine( "########" );
+                Console.WriteLine(  "$$$$$$$" );
+            }   
+
+            
+        }
+    }
+}
