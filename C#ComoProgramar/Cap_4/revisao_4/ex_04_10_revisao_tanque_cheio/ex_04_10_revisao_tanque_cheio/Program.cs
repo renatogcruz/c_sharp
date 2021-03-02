5fa16bd5b75b37760fd3709112d7908959fedaef
renatogcruz <renatogcruz@hotmail.com>
2021-03-02 08:21:29 -0300

    c# - 4.10 km/litros

---
+++ C#ComoProgramar/Cap_4/revisao_4/ex_04_10_revisao_tanque_cheio/ex_04_10_revisao_tanque_cheio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_04_10_revisao_tanque_cheio
+{
+    // 4.10 - receba como entrada os km rodados e o combustível gasto e imprima quilometragem/tanque
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double km,            // quilômetros rodados
+                   tanque,        // combustível gasto
+                   quilometragem; // relação km/tanque
+
+
+            //
+            Console.Write( "Entre com a quilometragem rodada: " );
+            km = double.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o combustível gasto: ");
+            tanque = double.Parse(Console.ReadLine());
+
+            // relação quilometragem para cada tanque cheio
+            quilometragem = km / tanque;
+            Console.WriteLine("A relação de quilometragem para o tanque é de " 
+                + quilometragem 
+                + " Km por litros");
+
+
+        }
+    }
+}
