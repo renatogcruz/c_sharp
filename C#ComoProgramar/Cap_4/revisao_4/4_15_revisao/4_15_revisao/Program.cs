078ecbfd278a4ffd2d3089153b356ca129c87eb7
renatogcruz <renatogcruz@hotmail.com>
2021-03-09 09:34:55 -0300

    c# progroma fatorial

---
+++ C#ComoProgramar/Cap_4/revisao_4/4_15_revisao/4_15_revisao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_15_revisao
+{
+    // criptografando dados
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dado,         // entrada de dados dos usuários
+                substitua,    // substitue os dados acima pela regra do problema;
+                troca;        // troca os digitos substituidos acima 
+
+            Console.Write("Entre com os dados (4 digitos): ");
+            dado = Int32.Parse(Console.ReadLine());
+
+            // substitua cada dígito pela soma desse dígito mais 7
+           
+
+            // troque o primeiro dígito pelo terceiro e troque o segundo dígito pelo quarto
+
+
+            // imprima o inteiro criptografado
+
+
+        }
+    }
+}
---
+++ C#ComoProgramar/Cap_4/revisao_4/4_16_revisao_fatorial/4_16_revisao_fatorial/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_16_revisao_fatorial
+{
+    
+    // calculando fatorial
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, 
+                number, 
+                fact;
+
+            // Entrada de dados pelo usuário
+            Console.Write("Entre com valor: ");
+            number = int.Parse(Console.ReadLine());
+
+
+            fact = number;
+            for (i = number - 1; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+
+
+            Console.WriteLine("\nO Fatorial do número digitado é: " + fact);
+            Console.ReadLine();
+                       
+        }
+    }
+}
