35d26804f57b2b7323deada1b42354d17c9a6c50
renatogcruz <renatogcruz@hotmail.com>
2021-03-10 16:23:26 -0300

    c# como programar - repetição controlada

---
+++ C#ComoProgramar/Cap_5/5_1_whileCounter/5_1_whileCounter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_1_whileCounter
+{
+    // Fig. 5.1: WhileCounter.cs
+    // Repetição controlada por contador
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inicialização
+            int counter = 1;
+
+            // condição de repetição
+            while (counter <= 5)
+            {
+                Console.WriteLine(counter);
+                // incrementa
+                counter++;
+            } // fim do while
+        } // fim do método Main
+    } // fim da classe Program
+}
