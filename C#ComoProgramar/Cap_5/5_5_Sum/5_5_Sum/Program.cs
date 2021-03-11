7091b67702dd8693ff82e8ea2845853b3ceff501
renatogcruz <renatogcruz@hotmail.com>
2021-03-11 07:54:22 -0300

    c# - Interest.cs program

---
+++ C#ComoProgramar/Cap_5/5_5_Sum/5_5_Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_5_Sum
+{
+    // Fig.: Sum.cs
+    // Soma com a estrutura for.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int number = 2; number <= 100; number += 2)
+                sum += number;
+
+            Console.WriteLine("The sum is " + sum);
+
+        } // fim do metodo Main
+    } // fim da classe Sum
+}
---
+++ C#ComoProgramar/Cap_5/5_8_Interest/5_8_Interest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_8_Interest
+{
+    // Fig. 5.8: Interesest.cs
+    // Calculando juros compostos
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal amount, principal = (decimal)1000.00;
+            double rate = 0.50;
+            string output;
+
+            output = "Year\tAmount on deposit\n";
+
+            for (int year = 1; year <= 10; year++)
+            {
+                amount = principal *
+                    (decimal)Math.Pow(1.0 + rate, year);
+
+                output += year + "\t" +
+                    String.Format("{0:C}", amount) + "\n";
+            }
+
+            Console.WriteLine(output, "Compound Interest");
+        }
+    }
+}
