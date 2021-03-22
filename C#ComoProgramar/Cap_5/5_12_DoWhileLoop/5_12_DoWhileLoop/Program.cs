c4840b41c978051402623a65380ca750bca6cb44
renatogcruz <renatogcruz@hotmail.com>
2021-03-22 14:27:26 -0300

    teste do/while loop

---
+++ C#ComoProgramar/Cap_5/5_12_DoWhileLoop/5_12_DoWhileLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_12_DoWhileLoop
+{
+    // Fig. 5.12: DoWhileLoop.cd
+    // A estrutura de repetição do/while
+
+    class DoWhileLoop
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+
+            do
+            {
+                Console.WriteLine(counter);
+                counter++;
+            } while (counter <= 5);
+
+        } // fim do método Main
+    } // fim da classe DoWhileLoop
+}
