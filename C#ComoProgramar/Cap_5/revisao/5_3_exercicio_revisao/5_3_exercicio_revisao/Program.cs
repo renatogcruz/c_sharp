7e2d13f0553f7513f7d34e557da55b61511c74c8
renatogcruz <renatogcruz@hotmail.com>
2021-03-24 09:37:54 -0300

    exercicios de revisão cap. 05

---
+++ C#ComoProgramar/Cap_5/revisao/5_3_exercicio_revisao/5_3_exercicio_revisao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_3_exercicio_revisao
+{
+    // Exercicios de revisão capítulo 5 - página 195
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // some inteiros ímpares entre 1 e 99 usando a estrutura for
+            int sum,
+                count;
+
+            sum = 0;            
+
+            for (count = 1; count <= 99; count += 2)
+            {
+                sum += count;
+            }
+
+            Console.WriteLine("Soma = " + sum);
+        }
+    }
+}
