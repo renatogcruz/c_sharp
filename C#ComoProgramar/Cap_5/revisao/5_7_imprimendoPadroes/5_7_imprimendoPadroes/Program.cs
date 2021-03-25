4683a9893f4755bd38e260f2883ff9bd00c20ec0
renatogcruz <renatogcruz@hotmail.com>
2021-03-25 14:25:37 -0300

    c# exercicios for

---
+++ C#ComoProgramar/Cap_5/revisao/5_7_imprimendoPadroes/5_7_imprimendoPadroes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_7_imprimendoPadroes
+{
+    // imprimindo asteriscos
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont;
+
+            for (cont = 1; cont <= 10; cont++)
+                Console.Write('*');
+        }
+    }
+}
