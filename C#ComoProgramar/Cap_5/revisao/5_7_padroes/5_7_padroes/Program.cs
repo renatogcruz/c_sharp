af73dc5eb8cd3981ec2844399ef6d3dd05a0b523
renatogcruz <renatogcruz@hotmail.com>
2021-03-26 20:00:28 -0300

    exercicíos FOR

---
+++ C#ComoProgramar/Cap_5/revisao/5_7_padroes/5_7_padroes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_7_padroes
+{
+    class Program
+    {
+        // Imprimindo padrões
+
+        static void Main(string[] args)
+        {
+            // item A exercício 5.7 - página 196
+
+            int cont;
+            string sinal;
+
+            sinal = "*";
+
+            for (cont = 1; cont <= 10; cont++)
+                Console.Write(sinal);            
+                
+
+                                               
+        }
+    }
+}
