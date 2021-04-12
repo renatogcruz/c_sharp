4b4d920b984f5f47a81b7292624e32c5d238c7bd
renatogcruz <renatogcruz@hotmail.com>
2021-04-12 08:37:11 -0300

    exemplo programa sequencia

---
+++ algoritmos_manzano_oliveira/Cap_3_sequencia/Exemplo_1/Exemplo_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_1
+{
+    /* Desenvolver programa que efetue a leitura de dois valores inteiros.
+     * Processe a operação de adição dos valores e apresente a soma.  
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first,
+                second;
+            int x,
+                a,
+                b;
+
+
+            // Entradas
+            Console.Write("Entre com primeiro número: ");
+            first = Console.ReadLine();
+
+            Console.Write("Entre com o segundo número: ");
+            second = Console.ReadLine();
+
+            // convertendo números string para int
+            a = Int32.Parse(first);
+            b = Int32.Parse(second);
+
+            // Processamento
+            x = a + b;
+
+            // Exibição
+            Console.WriteLine("A soma é {0}.", x);
+        }
+    }
+}
