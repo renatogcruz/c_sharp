14ee86cbb4f53fe34acb615e86fa19e64ba62dc3
renatogcruz <renatogcruz@hotmail.com>
2021-04-13 09:17:46 -0300

    exercicios de algoritmos

---
+++ algoritmos_manzano_oliveira/Cap_3_sequencia/programa_TEMPERATURA/programa_TEMPERATURA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_TEMPERATURA
+{
+    class Program
+    {
+        
+        // ler temperatura em ºC e converter para F
+
+        static void Main(string[] args)
+        {
+            string celsius;
+                
+            double c, // temperatura em ºC
+                f;    // temperatura em F
+
+            // Entrada
+            Console.Write("Entre com a temperatura em ºC: ");
+            celsius = Console.ReadLine();
+
+            // convertendo string para double
+            c = Convert.ToDouble(celsius);
+
+            // Processamento
+            f = c * 9 / 5 + 32;
+
+            // Exibição
+            Console.WriteLine("A temperatura é {0} F.", f);
+
+        }
+    }
+}
---
+++ algoritmos_manzano_oliveira/Cap_3_sequencia/programa_VOLUME/programa_VOLUME/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_VOLUME
+{
+    class Program
+    {
+
+        // calcular e apresentar volume de lata de óleo
+
+        static void Main(string[] args)
+        {
+
+            string altura, raio;
+
+            double v, r, h;
+
+            // Entrada
+            Console.Write("Entre com o raio da lata: ");
+            raio = Console.ReadLine();
+
+            Console.Write("Entre com a altura da lata: ");
+            altura = Console.ReadLine();
+
+            // convertendo string para double
+            r = Convert.ToDouble(raio);
+            h = Convert.ToDouble(altura);
+
+            // Processamento
+            v = 3.14 * Math.Pow(r, 2) * h;
+
+            // Exibição
+            Console.WriteLine("O volume da lata é de {0}.", v);
+
+        }
+    }
+}
