448bea7d3c2e71951630bdd35125a3b3976ab095
renatogcruz <renatogcruz@hotmail.com>
2021-04-14 09:20:43 -0300

    exercicios algoritmos sequencial

---
+++ algoritmos_manzano_oliveira/Cap_3_sequencia/programa_CONVERSÃO/programa_CONVERSÃO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_CONVERSÃO
+{
+    // programa converte dólar para real
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cotacao, dolar;
+
+            double c, d, r;
+
+            // Entrada
+            Console.Write("Entre com valor em dólares: ");
+            dolar = Console.ReadLine();
+
+            Console.Write("Entre com a cotação do dólar: ");
+            cotacao = Console.ReadLine();
+
+            // convertendo string para double
+            d = Convert.ToDouble(dolar);
+            c = Convert.ToDouble(cotacao);
+
+            // Processamento
+            r = d * c;
+
+            // Exibição
+            Console.WriteLine("O valor em real é de R$ {0}", r);
+        }
+    }
+}
---
+++ algoritmos_manzano_oliveira/Cap_3_sequencia/programa_VALORES_TROCADOS/programa_VALORES_TROCADOS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_VALORES_TROCADOS
+{
+    class Program
+    {
+        // leia dois valores e troque para que um possua o valor do outro
+
+        static void Main(string[] args)
+        {
+            string valor_a, valor_b;
+
+            int a, b, x;
+
+            // Entrada
+            Console.Write("Entre com valor de A: ");
+            valor_a = Console.ReadLine();
+
+            Console.Write("Entre com valor de B: ");
+            valor_b = Console.ReadLine();
+
+            // convertendo string para integer
+            a = Int32.Parse(valor_a);
+            b = Int32.Parse(valor_b);
+
+            // Processamento
+            x = a;
+            a = b;
+            b = x;
+
+            // Exibição
+            Console.WriteLine("A = {0}. B = {1}.", a, b);
+        }
+    }
+}
