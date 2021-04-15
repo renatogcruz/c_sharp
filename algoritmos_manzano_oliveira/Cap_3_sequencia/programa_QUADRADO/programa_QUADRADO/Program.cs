f8434d37d7b390396718041d0f19be6d73954eee
renatogcruz <renatogcruz@hotmail.com>
2021-04-15 08:33:19 -0300

    exercicios algoritmos sequencial

---
+++ algoritmos_manzano_oliveira/Cap_3_sequencia/programa_QUADRADO/programa_QUADRADO/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_QUADRADO
+{
+    // leia 2 valores (A, B) e apresente o quadrado do resultado da divisão de A por B
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberA, numberB;
+
+            double a, b, d, r;
+
+            // Entrada
+            Console.Write("Entre com valor de A: ");
+            numberA = Console.ReadLine();
+
+            Console.Write("Entre com valor de B: ");
+            numberB = Console.ReadLine();
+
+            // convertendo string para integer
+            a = Convert.ToDouble(numberA);
+            b = Convert.ToDouble(numberB);
+
+            // Processamento
+            d = a / b;
+            r = Math.Pow(d, 2);
+
+            // Exibição
+            Console.WriteLine("O quadrado da divisão é igual a {0}", r);
+
+        }
+    }
+}
---
+++ algoritmos_manzano_oliveira/Cap_3_sequencia/programa_SUCESSOR_ANTECESSOR/programa_SUCESSOR_ANTECESSOR/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_SUCESSOR_ANTECESSOR
+{
+    // ler um valor e apresentar seu antecessor e seu sucessor
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor;
+
+            int v, s, a;
+
+            // Entrada
+            Console.Write("Entre com um valor: ");
+            valor = Console.ReadLine();
+
+            // convertendo string para integer
+            v = Int32.Parse(valor);
+
+            // Processamento
+            s = v + 1;
+            a = v - 1;
+
+            // Exibição
+            Console.WriteLine("Antecessor = {0}. Sucessor = {1}.", a, s);
+        }
+    }
+}
