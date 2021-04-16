0113efb7bc7a781978ebc968697e774c7ebde8a0
renatogcruz <renatogcruz@hotmail.com>
2021-04-16 08:22:03 -0300

    exercicios algoritmo sequencial

---
+++ algoritmos_manzano_oliveira/Cap_3_sequencia/programa_QUDRADO_SOMA/programa_QUDRADO_SOMA/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_QUDRADO_SOMA
+{
+    // Leia 3 valores e apresente o quadrado de suas somas (ex. 4.m - pág. 65)
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor_a, valor_b, valor_c;
+            double a, b, c, r;
+
+            // Entrada
+            Console.Write("Entre com valor de A: ");
+            valor_a = Console.ReadLine();
+
+            Console.Write("Entre com valor de B: ");
+            valor_b = Console.ReadLine();
+
+            Console.Write("Entre com valor de C: ");
+            valor_c = Console.ReadLine();
+
+            // convertendo string em integer
+            a = Convert.ToDouble(valor_a);
+            b = Convert.ToDouble(valor_b);
+            c = Convert.ToDouble(valor_c);
+
+            // Processamento
+            r = Math.Pow((a + b + c), 2);
+
+            // Exibição
+            Console.WriteLine("O valor do quadrado é {0}.", r);
+
+
+        }
+    }
+}
