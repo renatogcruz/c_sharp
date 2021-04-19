cc3cc407f54311b0c3ad00018b687652b0ba71d2
renatogcruz <renatogcruz@hotmail.com>
2021-04-19 08:29:19 -0300

    exercicios algoritmos de decisão

---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/exemplo_1/exemplo_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_1
+{
+    // Exemplo 1 - página 101
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+
+            double n, r;
+
+            // Entrada
+            Console.Write("Entre com valor: ");
+            number = Console.ReadLine();
+
+            n = Convert.ToDouble(number);
+
+            // Processamento
+            if (n > 0)
+                if (n != 5)
+                    Console.Write("O valor é {0}", r = Math.Pow(n, (1 / 2)));
+            
+                else
+                    Console.Write("O valor é {0}", r = Math.Pow(n, (1 / 3)));
+
+                
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/exemplo_2/exemplo_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_2
+{
+    // Exmplo 2 página 102
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberA, numberB, numberC;
+            int a, b, c, x;
+
+            // Entrada
+            Console.Write("Entre com peso de A: ");
+            numberA = Console.ReadLine();
+
+            Console.Write("Entre com peso de B: ");
+            numberB = Console.ReadLine();
+
+            Console.Write("Entre com peso de C: ");
+            numberC = Console.ReadLine();
+
+            // converntendo string para integer
+            a = Int32.Parse(numberA);
+            b = Int32.Parse(numberB);
+            c = Int32.Parse(numberC);
+
+            // Processamento
+            x = a;
+            if (x < b)
+                x = b;
+            if (x < c)
+                x = c;
+
+            // Exibição
+            Console.WriteLine("Maior peso = {0}", x);
+        }
+    }
+}
