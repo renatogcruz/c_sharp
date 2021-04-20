f7c6ced15cc8a12029b4495a2509b06a80237b0b
renatogcruz <renatogcruz@hotmail.com>
2021-04-20 08:47:40 -0300

    exercicios algoritmos decisão

---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/exemplo_3/exemplo_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_3
+{
+    // Exemplo 3 - página 103
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberA, numberB, numberC;
+            double a, b, c;
+
+            // Entrada
+            Console.Write("Entre com lado A: ");
+            numberA = Console.ReadLine();
+
+            Console.Write("Entre com lado B: ");
+            numberB = Console.ReadLine();
+
+            Console.Write("Entre com lado C: ");
+            numberC = Console.ReadLine();
+
+            // convertendo string para double
+            a = Convert.ToDouble(numberA);
+            b = Convert.ToDouble(numberB);
+            c = Convert.ToDouble(numberC);
+
+            // Processamento
+            if (a < b) & (a > c);
+            {
+                if (b == a) & (b > c);
+                    Console.WriteLine("Texto");
+            }
+                    
+
+        }
+    }
+}
---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/exemplo_4/exemplo_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_4
+{
+    // exemplo 4 página 105
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            int n, r2, r3;
+
+            // Entrada
+            Console.Write("Entre com valor: ");
+            number = Console.ReadLine();
+
+            // convertendo string para integer
+            n = Int32.Parse(number);
+
+            // Processamento
+            r2 = n % 2;
+            r3 = n % 3;
+
+            if (r2 == 0 || r3 == 0)
+                     Console.WriteLine(n);           
+            
+
+        }
+    }
+}
