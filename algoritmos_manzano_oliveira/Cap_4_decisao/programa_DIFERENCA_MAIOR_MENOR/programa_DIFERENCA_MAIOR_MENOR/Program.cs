cbaba7584a5d33e6dce94c8eb0f0ebce6f5e9232
renatogcruz <renatogcruz@hotmail.com>
2021-04-21 08:14:39 -0300

    exercicios algoritmos decisão

---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/programa_DIFERENCA_MAIOR_MENOR/programa_DIFERENCA_MAIOR_MENOR/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_DIFERENCA_MAIOR_MENOR
+{
+    // exercicio 3.a - página 108
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberA, numberB;
+            int a, b, r;
+
+            // Entrada
+            Console.Write("Entre com valor de A: ");
+            numberA = Console.ReadLine();
+
+            Console.Write("Entre com valor de B: ");
+            numberB = Console.ReadLine();
+
+            // converter string para integer
+            a = Int32.Parse(numberA);
+            b = Int32.Parse(numberB);
+
+            // Processamento
+            if (a > b)
+                r = a - b;
+            else
+                r = b - a;
+
+            // Exibição
+            Console.WriteLine("A diferença é de {0}", r);
+        }
+    }
+}
---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/programa_POSITIVO/programa_POSITIVO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_POSITIVO
+{
+    // exercício 3.b - pág. 108
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            int n;
+
+            // Entrada
+            Console.Write("Entre com valor inteiro (positivo ou negativo): ");
+            number = Console.ReadLine();
+
+            // convertendo string para integer
+            n = Int32.Parse(number);
+
+            // Processamento
+            if (n < 0)
+                n = n * -1;
+
+            // Exibição
+            Console.WriteLine("O valor é {0}", n);
+        }
+    }
+}
