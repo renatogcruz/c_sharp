74f6ef0bd774879fd8fc49bcadec5f86f62ded50
renatogcruz <renatogcruz@hotmail.com>
2021-04-24 08:35:02 -0300

    exercicios algoritmos decisão

---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/programa_BHASKARA/programa_BHASKARA/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_BHASKARA
+{
+    // ex. 3.e - pág. 109
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberA, numberB, numberC;
+            double a, b, c, d, x1, x2;
+
+            // Entrada
+            Console.Write("Entre com primeiro valor: ");
+            numberA = Console.ReadLine();
+
+            Console.Write("Entre com segundo valor: ");
+            numberB = Console.ReadLine();
+
+            Console.Write("Entre com terceiro valor: ");
+            numberC = Console.ReadLine();
+
+            // convertendo string para double
+            a = Convert.ToDouble(numberA);
+            b = Convert.ToDouble(numberB);
+            c = Convert.ToDouble(numberC);
+
+            // Processamento
+            if (a == 0 && b == 0 && c == 0)
+            {
+                Console.WriteLine("Não há solução!");
+            }
+
+            d = Math.Pow(b, 2) - 4 * a * c;
+            if (d == 0)
+            {
+                x1 = -b / (2 * a);
+                Console.WriteLine(x1);
+            }
+            else
+            {
+                if (d > 0)
+                {
+                    x1 = -b / (2 * a);
+                    x2 = b / (2 * a);
+                    Console.WriteLine(x1);
+                    Console.WriteLine(x2);
+                }
+                else
+                {
+                    Console.WriteLine("Não existe raizes reais!");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/programa_CRESCENTE/programa_CRESCENTE/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_CRESCENTE
+{
+    // Ex. 3.f - pág. 109
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberA, numberB, numberC;
+            int a, b, c, x;
+
+            // Entrada
+            Console.Write("Entre com primeiro valor: ");
+            numberA = Console.ReadLine();
+
+            Console.Write("Entre com segundo valor: ");
+            numberB = Console.ReadLine();
+
+            Console.Write("Entre com terceiro valor: ");
+            numberC = Console.ReadLine();
+
+            // convertendo string -> integer
+            a = Int32.Parse(numberA);
+            b = Int32.Parse(numberB);
+            c = Int32.Parse(numberC);
+
+            // Processamento
+            if (a > b)
+            {
+                x = a;
+                a = b;
+                b = x;
+            }
+            if (a > c)
+            {
+                x = a;
+                a = c;
+                c = x;
+            }
+            if (b > c)
+            {
+                x = b;
+                b = c;
+                c = x;
+            }
+
+            // Exibição
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+        }
+    }
+}
