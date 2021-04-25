b9080ddcaa2a0771515c508d49515475ed3a4bb6
renatogcruz <renatogcruz@hotmail.com>
2021-04-25 08:44:28 -0300

    exercicios algoritmos decisão

---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/programa_DIVISIVIEIS_AND/programa_DIVISIVIEIS_AND/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_DIVISIVIEIS_AND
+{
+    // exercícios 3.g - pág. 109
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberA, numberB, numberC, numberD;
+            int a, b, c, d, resto2, resto3;
+
+            // Entrada
+            Console.Write("Entre com valor de A: ");
+            numberA = Console.ReadLine();
+
+            Console.Write("Entre com valor de B: ");
+            numberB = Console.ReadLine();
+
+            Console.Write("Entre com valor de C: ");
+            numberC = Console.ReadLine();
+
+            Console.Write("Entre com valor de D: ");
+            numberD = Console.ReadLine();
+
+            // converter string em Integer
+            a = Int32.Parse(numberA);
+            b = Int32.Parse(numberB);
+            c = Int32.Parse(numberC);
+            d = Int32.Parse(numberD);
+
+            // Processamento
+            resto2 = a % 2;
+            resto3 = a % 3;
+            if (resto2 == 0 && resto3 == 0)
+            {
+                Console.WriteLine(a);
+            }
+
+            resto2 = b % 2;
+            resto3 = b % 3;
+            if (resto2 == 0 && resto3 == 0)
+            {
+                Console.WriteLine(b);
+            }
+
+            resto2 = c % 2;
+            resto3 = c % 3;
+            if (resto2 == 0 && resto3 == 0)
+            {
+                Console.WriteLine(c);
+            }
+
+            resto2 = d % 2;
+            resto3 = d % 3;
+            if (resto2 == 0 && resto3 == 0)
+            {
+                Console.WriteLine(d);
+            }
+
+
+        }
+    }
+}
