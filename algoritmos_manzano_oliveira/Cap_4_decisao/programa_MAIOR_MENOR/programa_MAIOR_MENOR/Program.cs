580a0eed71d22a35742d1f3e7d2aaedfb4febabf
renatogcruz <renatogcruz@hotmail.com>
2021-04-26 07:55:40 -0300

    exercicios algoritmos decisão

---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/programa_MAIOR_MENOR/programa_MAIOR_MENOR/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_MAIOR_MENOR
+{
+    // ex. 3.i - página 109
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberA, numberB, numberC, numberD, numberE;
+            int a, b, c, d, e, maior, menor;
+
+            // Entrada
+            Console.Write("Entre com valor de A: ");
+            numberA = Console.ReadLine();
+
+            Console.Write("Entre com valor de B: ");
+            numberB = Console.ReadLine();
+
+            Console.Write("Entre com valor de C: ");
+            numberC = Console.ReadLine();
+
+            Console.Write("Entre com valor de D: ");
+            numberD = Console.ReadLine();
+
+            Console.Write("Entre com valor de E: ");
+            numberE = Console.ReadLine();
+
+            // convertendo string em integer
+            a = Int32.Parse(numberA);
+            b = Int32.Parse(numberB);
+            c = Int32.Parse(numberC);
+            d = Int32.Parse(numberD);
+            e = Int32.Parse(numberE);
+
+            // Processamento
+            maior = a;
+            if (maior < b)
+            {
+                maior = b;
+            }
+            if (maior < c)
+            {
+                maior = c;
+            }
+            if (maior < d)
+            {
+                maior = d;
+            }
+            if (maior < e)
+            {
+                maior = e;
+            }
+
+            menor = a;
+            if (menor > b)
+            {
+                menor = b;
+            }
+            if (menor > c)
+            {
+                menor = c;
+            }
+            if (menor > d)
+            {
+                menor = d;
+            }
+            if (menor > e)
+            {
+                menor = e;
+            }
+
+            // Exibição
+            Console.WriteLine("Maior: {0}. Menor: {1}", maior, menor);
+
+        }
+    }
+}
---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/programa_PAR_IMPAR/programa_PAR_IMPAR/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_PAR_IMPAR
+{
+    // ex. 3.j - página 109
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            int n, resto2;
+
+            // Entrada
+            Console.Write("Entre com valor de N: ");
+            number = Console.ReadLine();
+
+            // convertendo string em integer
+            n = Int32.Parse(number);
+
+            // Processamento
+            resto2 = n % 2;
+            if (resto2 == 0)
+            {
+                Console.WriteLine("{0} é par!", n);
+            }
+            else
+            {
+                Console.WriteLine("{0} é ímpar!", n);
+            }
+            
+
+        }
+    }
+}
