e37c774ec05d4f398c24647fa7dbe526440286aa
renatogcruz <renatogcruz@hotmail.com>
2021-04-27 08:11:13 -0300

    exercicios algoritmos de decisão

---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/programa_MENOR/programa_MENOR/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_MENOR
+{
+    // Ex. 3.l - página 109
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            int n;
+
+            // Entrada
+            Console.Write("Entre com valor qualquer: ");
+            number = Console.ReadLine();
+
+            // convertendo string em inteiro
+            n = Int32.Parse(number);
+
+            // Processamento
+            if (! (n > 3))
+            {
+                Console.WriteLine(n);
+            }
+
+       
+        }
+    }
+}
---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/programa_PERMITIDO/programa_PERMITIDO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_PERMITIDO
+{
+    // Ex. 3.k - página 109
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            int n;
+
+            // Entrada
+            Console.Write("Entre com valor (1 até 9): ");
+            number = Console.ReadLine();
+
+            // convertendo
+            n = Int32.Parse(number);
+
+            // Processamento
+            if (n > 0 && n < 10)
+            {
+                Console.WriteLine("Valor dentro da faixa permitida!");
+            }
+            else
+            {
+                Console.WriteLine("Valor fora da faixa permitida!");
+            }
+        }
+    }
+}
