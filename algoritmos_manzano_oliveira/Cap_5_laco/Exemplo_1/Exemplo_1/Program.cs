cb593b3825b438777b94b9996f80b7432ef61f6f
renatogcruz <renatogcruz@hotmail.com>
2021-04-30 08:29:25 -0300

    algoritmos de laço - exercícios

---
+++ algoritmos_manzano_oliveira/Cap_5_laco/Exemplo_1/Exemplo_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_1
+{
+    // Exemplo 1 - página 136
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fat, cont;
+
+            // processamento
+            fat = 1;
+            cont = 1;
+
+            while (cont <= 5)
+            {
+                fat = fat * cont;
+                cont = cont + 1;
+            }
+
+            // exibição
+            Console.WriteLine("5! = {0}", fat);
+
+        }
+    }
+}
---
+++ algoritmos_manzano_oliveira/Cap_5_laco/exemplo_2/exemplo_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fat, cont;
+
+            // processamento
+            fat = 1;
+            cont = 1;
+
+            while (! (cont > 5))
+            {
+                fat = fat * cont;
+                cont = cont + 1;
+            }
+
+            // exibição
+            Console.WriteLine("5! = {0}", fat);
+        }
+    }
+}
