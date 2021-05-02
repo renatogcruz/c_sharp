1117bb6b4eef20ed75a7e5122ccf24a0ddb0e273
renatogcruz <renatogcruz@hotmail.com>
2021-05-02 09:07:43 -0300

    exercícios de algoritmo de laço

---
+++ algoritmos_manzano_oliveira/Cap_5_laco/programa_QUADRADO/programa_QUADRADO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_QUADRADO
+{
+    // exercício A - página 141
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cont, qua;
+
+            cont = 15;
+
+            while (cont <= 200)
+            {
+                qua = Math.Pow(cont, 2);
+                Console.WriteLine("{0}² = {1}", cont, qua);
+
+                cont += 1;
+            }
+        }
+    }
+}
