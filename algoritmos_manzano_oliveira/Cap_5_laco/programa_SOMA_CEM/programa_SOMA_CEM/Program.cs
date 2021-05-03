824fc6b1661825f4ec20a6f477b70a8399eaff99
renatogcruz <renatogcruz@hotmail.com>
2021-05-03 07:49:27 -0300

    exercícios algoritmos de laço

---
+++ algoritmos_manzano_oliveira/Cap_5_laco/programa_SOMA_CEM/programa_SOMA_CEM/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_SOMA_CEM
+{
+    // exercício C - página 141
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont, soma;
+
+            // Processamento
+            soma = 0;
+            cont = 1;
+
+            while (cont <= 100)
+            {
+                soma = soma + cont;
+                cont = cont + 1;
+            }
+
+            Console.WriteLine("A soma dos 100 primeiros números é igual a {0}", soma);
+        }
+    }
+}
---
+++ algoritmos_manzano_oliveira/Cap_5_laco/programa_TABUADA/programa_TABUADA/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_TABUADA
+{
+    // Exercício B - página 141
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            int n, cont, tab;
+
+            // Entrada
+            Console.Write("Entre com valor de N: ");
+            number = Console.ReadLine();
+
+            // convertendo String em Integer
+            n = Int32.Parse(number);
+
+            // Processamento
+            cont = 0;
+
+            while (cont <= 10)
+            {
+                tab = n * cont;
+                Console.WriteLine("{0} x {1} = {2}", n, cont, tab);
+                cont = cont + 1;
+            }
+
+
+
+        }
+    }
+}
