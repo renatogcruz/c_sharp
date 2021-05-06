3768441111af0d3f58a3d04f37e162f57b5b2605
renatogcruz <renatogcruz@hotmail.com>
2021-05-06 07:28:40 -0300

    exercício algoritmo laço

---
+++ algoritmos_manzano_oliveira/Cap_5_laco/programa_DIVISIVEL_4/programa_DIVISIVEL_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_DIVISIVEL_4
+{
+    // Exercício f - página 141
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 1;
+            int resto;
+
+            // Processamento
+            while (cont <= 199)
+            {
+                resto = cont % 4;
+                if (resto == 0)
+                {
+                    Console.WriteLine(cont);
+                }
+                cont += 1;
+            }
+        }
+    }
+}
