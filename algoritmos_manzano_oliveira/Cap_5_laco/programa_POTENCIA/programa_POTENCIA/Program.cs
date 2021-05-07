456d9605970939930fbb293b97031c1b7044ced4
renatogcruz <renatogcruz@hotmail.com>
2021-05-07 09:58:35 -0300

    exercicios algoritmos laço

---
+++ algoritmos_manzano_oliveira/Cap_5_laco/programa_POTENCIA/programa_POTENCIA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_POTENCIA
+{
+    // Exercício F - página 141
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b = 3;
+            double cont = 0;
+            double resultado;
+
+            // Processamento
+            while (cont <= 15)
+            {
+                resultado = Math.Pow(b, cont);
+                Console.WriteLine("{0} elevado a {1} = {2}", b, cont, resultado);
+                cont += 1;
+            }
+
+
+        }
+    }
+}
