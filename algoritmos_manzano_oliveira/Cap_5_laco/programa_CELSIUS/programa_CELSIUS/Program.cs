4a4cd344a49277f295fd649701f428bb3bb7f230
renatogcruz <renatogcruz@hotmail.com>
2021-05-11 08:42:25 -0300

    exercício algoritmo laço

---
+++ algoritmos_manzano_oliveira/Cap_5_laco/programa_CELSIUS/programa_CELSIUS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_CELSIUS
+{
+    // Exercício J - página 142
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 10;
+
+            // processamento
+            while (c <= 100)
+            {
+                int f = (c * 9 / 5) + 32;
+                Console.WriteLine("{0} ºC. {1} ºF", c, f);
+                c += 10;
+            }
+        }
+    }
+}
