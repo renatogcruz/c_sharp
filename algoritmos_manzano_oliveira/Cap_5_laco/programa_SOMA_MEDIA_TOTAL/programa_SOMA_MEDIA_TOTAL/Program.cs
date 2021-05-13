5de2a1532abc41551d252a99d3507fc5bd5afcdc
renatogcruz <renatogcruz@hotmail.com>
2021-05-13 08:37:52 -0300

    exercicio algoritmo laço

---
+++ algoritmos_manzano_oliveira/Cap_5_laco/programa_SOMA_MEDIA_TOTAL/programa_SOMA_MEDIA_TOTAL/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_SOMA_MEDIA_TOTAL
+{
+    // exercício N - página 142
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada
+            string number;
+            int total = 0;
+            int soma = 0;
+            int n = 0;
+            int media = 0;
+
+            // Processamento
+            while (n >= 0)
+            {
+                Console.Write("Entre com valor: ");
+                number = Console.ReadLine();
+
+                // converntendo string para inteiro
+                n = Int32.Parse(number);
+
+                if (n >= 0)
+                {
+                    soma = soma + n;
+                    total = total + 1;
+                }
+                                
+            }
+
+            if (total > 0)
+            {
+                media = soma / total;
+            }
+
+            Console.WriteLine("Média = {0}. Total = {1}. Soma = {2}", media, total, soma);
+                         
+        }
+    }
+}
