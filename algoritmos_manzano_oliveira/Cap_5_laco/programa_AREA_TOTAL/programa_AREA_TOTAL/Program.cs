b64355811e0b4ebd24acdbb40e59a39a8e841178
renatogcruz <renatogcruz@hotmail.com>
2021-05-19 09:09:23 -0300

    exercício algoritmo laço

---
+++ algoritmos_manzano_oliveira/Cap_5_laco/programa_AREA_TOTAL/programa_AREA_TOTAL/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_AREA_TOTAL
+{
+    class Program
+    {
+        // Exercício Q - página 142
+
+        static void Main(string[] args)
+        {
+            // Entrada
+            double total = 0;
+            double c, l, a;
+            string continuar = "s";
+            string nome, comprimento, largura;
+
+            // Processamento
+            while (continuar == "s")
+            {
+                Console.Write("Entre com o nome: ");
+                nome = Console.ReadLine();
+
+                Console.Write("Entre com a largura: ");
+                largura = Console.ReadLine();
+
+                Console.Write("Entre com o comprimento: ");
+                comprimento = Console.ReadLine();
+
+                // convertendo string para double
+                l = Convert.ToDouble(largura);
+                c = Convert.ToDouble(comprimento);
+
+                a = l * c;
+                total = total + a;
+                Console.Write("Deseja continuar?" );
+                continuar = Console.ReadLine();
+
+            }
+
+            // Exibição
+            Console.WriteLine("Total de área = {0} m²", total);
+        }
+    }
+}
