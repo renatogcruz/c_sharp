8721de6a28164952ac610a6b50974ad1060e9498
renatogcruz <renatogcruz@hotmail.com>
2021-05-20 08:43:48 -0300

    exercício de algoritmo de laço

---
+++ algoritmos_manzano_oliveira/Cap_5_laco/programa_DIVISAO/programa_DIVISAO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_DIVISAO
+{
+    // exercício S - página 143
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dividendo, divisor;
+            int divid, divis;
+            int quociente = 1;
+
+            Console.Write("Entre com dividendo: ");
+            dividendo = Console.ReadLine();
+
+            Console.Write("Entre com divisor: ");
+            divisor = Console.ReadLine();
+
+            divid = Int32.Parse(dividendo);
+            divis = Int32.Parse(divisor);
+
+            while (divis < divid)
+            {
+                divid = divid - divis;
+                quociente = quociente + 1;
+            }
+            Console.WriteLine("{0} / {1} = {2}", dividendo, divisor, quociente);
+        }
+    }
+}
