fbe8282b57ac5e9ea78e2382efd526413f8944c4
renatogcruz <renatogcruz@hotmail.com>
2021-05-21 13:32:59 -0300

    exercício algoritmo laço

---
+++ algoritmos_manzano_oliveira/Cap_5_laco/programa_FATORIAL/programa_FATORIAL/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_FATORIAL
+{
+    // Exercício O - página 142
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j;
+            int r;
+            int fat;
+
+            // Processamento
+            while (i <= 10)
+            {
+                r = i % 2;
+                if (r != 0)
+                {
+                    fat = 1;
+                    j = 1;
+                    while (j <= i)
+                    {
+                        fat = fat * j;
+                        j = j + 1;
+                    }
+                    Console.WriteLine("{0}! = {1}", i, fat);
+                }
+
+                i = i + 1;
+            }
+            
+        }
+    }
+}
