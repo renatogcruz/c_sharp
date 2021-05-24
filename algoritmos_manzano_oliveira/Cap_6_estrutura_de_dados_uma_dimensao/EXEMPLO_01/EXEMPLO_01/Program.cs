e3ce3fb82b566521c2957ac805d3329d97752f5b
renatogcruz <renatogcruz@hotmail.com>
2021-05-24 08:41:55 -0300

    exercicío estrutura de dados de uma dimensão

---
+++ algoritmos_manzano_oliveira/Cap_6_estrutura_de_dados_uma_dimensao/EXEMPLO_01/EXEMPLO_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXEMPLO_01
+{
+    // Exemplo 1 - página 152
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int contador = 1; contador < 10; contador++)
+            {
+                Console.Write("Entre com valor: ");
+                string a = Console.ReadLine();
+            }
+
+            Console.WriteLine(a);
+
+        }
+    }
+}
