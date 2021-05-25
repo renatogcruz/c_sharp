cfba67fc15f799d3b7c888d9adaf0cd88ce6e599
renatogcruz <renatogcruz@hotmail.com>
2021-05-25 08:48:37 -0300

    algoritmos estruturas de dados de uma dimensão

---
+++ algoritmos_manzano_oliveira/Cap_6_estrutura_de_dados_uma_dimensao/EXEMPLO_1/EXEMPLO_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXEMPLO_1
+{
+    // Exemplo 1 - página 152
+
+    class Program
+    {
+        //Declaração da lista
+        List<int> valores = new List<int>();
+
+        static void Main(string[] args)
+        {
+
+            for (int cont = 1; cont < 2; cont++)
+            {
+                Console.Write("Entre com valor: ");
+                string valor = Console.ReadLine();
+
+                int a = Int32.Parse(valor);
+
+                //Adiciona o item à lista
+                valores.Add(a);
+
+
+            }
+
+        }
+    }
+}
