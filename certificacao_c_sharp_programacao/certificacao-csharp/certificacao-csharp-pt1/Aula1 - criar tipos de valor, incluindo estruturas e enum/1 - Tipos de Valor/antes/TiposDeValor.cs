97ef1172c366dc101e62c3c361171cfad0dc0540
renatogcruz <renatogcruz@hotmail.com>
2021-07-21 20:11:50 -0300

    Início certificação c# programming

---
+++ certificacao_c_sharp_programacao/certificacao-csharp/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/antes/TiposDeValor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.antes
+{
+    class TiposDeValor : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade;
+            idade = 30;            
+            Console.WriteLine(idade);
+            
+            int copiaIdade = idade;
+            // mouse sobre 'int' e veremos sua documentação
+            // int == System.Int32
+
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"Cópia idade: {copiaIdade}");
+
+            int? idade2 = null; // tipo int? é um tipo anulável
+            System.Nullable<int> Idade3 = null; // mesma declaração de cima
+
+        }
+    }
+}
---
+++ certificacao_c_sharp_programacao/certificacao-csharp/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/2 - Tipos Inteiros/antes/Inteiros.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.antes
+{
+    class TiposInteiros : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade = 15;
+            // idade = 15.5; // instrução errado, casas decimais em atribuição inteiro
+
+            char resposta = 'S'; // System.Char - ele armazena números
+
+            byte nivelDeAzul = 0xFF; // = 255 em decimal (só números positivos)
+
+            short passageirosVoo = 230; // System.Int16
+
+            int população = 1500; // System.Int32
+            
+            long populacaoBrasil = 207_660_929; // System.Int64
+
+            sbyte nivelDeBrilho = - 127; // System.Sbyte
+
+            ushort passageirosNavio = 230; // System.UInt16 (não aceita negativo)
+
+            uint estoque = 1500; // System.UInt32
+
+            ulong populacaoDoMundo = 7_000_000_000; // System.UInt64
+
+            
+
+           
+
+        }
+    }
+}
